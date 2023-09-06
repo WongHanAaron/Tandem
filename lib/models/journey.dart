53e99c445acc59609ffd08c2b6d2ca54e6401d21
AaronWong <aaronwonghan@gmail.com>
2023-09-05 20:45:32 -0700

    Started adding dashboard page. Added spannable grid package

---
+++ lib/models/journey.dart
@@ -0,0 +1,3 @@
+/// A journey contains the information for the tasks, events,
+/// goals and habits for a group of people
+class Journey {}
---
+++ lib/pages/dashboard/dashboardpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:spannable_grid/spannable_grid.dart';
+
+// Reference SpannableGrid: https://pub.dev/packages/spannable_grid
+class DashboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SpannableGrid(
+        cells: [SpannableGridCellData(id: "Test Cell 1", column: 1, row: 1)],
+        columns: 3,
+        rows: 3);
+  }
+}
