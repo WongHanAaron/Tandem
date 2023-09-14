75c2a89c1d1e1a5294d25286c8a5a2efe0289216
AaronWong <aaronwonghan@gmail.com>
2023-09-14 08:34:53 -0700

    Added implementation for user creation

--- ui/lib/models/user.dart
+++ ui/lib/models/user.dart
@@ -5,7 +5,7 @@ part 'user.g.dart';
 /// Describes a single user for this specific journey
 @collection
 class User {
-  Id id = Isar.autoIncrement;
+  int id = Isar.autoIncrement;
 
   String? firstName;
   String? middleName;
--- ui/lib/services/repositories/journey_repository.dart
+++ ui/lib/services/repositories/journey_repository.dart
@@ -1,16 +1,59 @@
+import 'package:isar/isar.dart';
 import 'package:tandem/models/change_tracking/property_change.dart';
 
-import '../models/user.dart';
+import '../../models/user.dart';
+import 'isar_provider.dart';
 
 /// Provides state information about this journey
 abstract class IJourneyRepository {
+  /// Startup the repository
+  void startUp();
+
   /// Updates the repository with the specific property changed
   void update(PropertyChange propertyChange);
 
   /// Create a new user from this passed in object
   void createNewUser(User user);
 
-  void removeUser(Id userId);
+  /// Removes the user at the specified user id
+  void removeUser(int userId);
+
+  Future<List<User>> getUsers({List<int>? userIds});
 }
 
-class JourneyRepository extends IJourneyRepository {}
+class JourneyRepository extends IJourneyRepository {
+  Isar? _isar;
+
+  @override
+  void startUp() async {
+    _isar = await IsarProvider().get();
+  }
+
+  @override
+  void createNewUser(User user) {
+    _isar!.users.put(user);
+  }
+
+  @override
+  void removeUser(int userId) {
+    _isar!.users.delete(userId);
+  }
+
+  @override
+  void update(PropertyChange propertyChange) {
+    // TODO: implement update
+  }
+
+  @override
+  Future<List<User>> getUsers({List<int>? userIds}) async {
+    if (userIds != null) {
+      final returned = await _isar!.users.getAll(userIds);
+      return <User>[
+        for (var s in returned)
+          if (s != null) s
+      ];
+    } else {
+      return _isar!.users.where().findAll();
+    }
+  }
+}
