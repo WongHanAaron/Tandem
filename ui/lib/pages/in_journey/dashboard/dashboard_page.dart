5e145d84a328c261f455f1db1fd4856d01493ede
AaronWong <aaronwonghan@gmail.com>
2023-09-19 23:40:42 -0700

    Added page base and added implementation for dashboard page

--- ui/lib/pages/in_journey/dashboard/dashboard_page.dart
+++ ui/lib/pages/in_journey/dashboard/dashboard_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:spannable_grid/spannable_grid.dart';
+import 'package:tandem/pages/in_journey/page_base.dart';
 
 // Reference SpannableGrid: https://pub.dev/packages/spannable_grid
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends PageBase {
   @override
   Widget build(BuildContext context) {
     return SpannableGrid(cells: [
@@ -13,4 +15,11 @@ class DashboardPage extends StatelessWidget {
           child: Row(children: [Text('Dashboard Page')]))
     ], columns: 3, rows: 3);
   }
+
+  @override
+  Widget getNavigationIcon(BuildContext context) =>
+      const Icon(Icons.dashboard_sharp);
+
+  @override
+  Widget getNavigationLabel(BuildContext context) => const Text("Dashboard");
 }
---
+++ ui/lib/pages/in_journey/page_base.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// The base class for a page within the in journey state
+abstract class PageBase extends StatelessWidget {
+  const PageBase({super.key});
+
+  /// gets the navigation icon
+  Widget getNavigationIcon(BuildContext context);
+
+  /// accesses the label for the navigation item
+  Widget getNavigationLabel(BuildContext context);
+}
